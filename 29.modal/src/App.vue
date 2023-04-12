2320cfeb80336621a794342165cb42dfd73bc9e6
yunhsuan <yunhsuan2017@gmail.com>
2023-04-12 17:36:19 +0800

    add29

---
+++ 29.modal/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="[
+    'w-screen h-screen',
+    'p-5',
+    'bg-red-200'
+  ]">
+    b
+    <Modal>
+      
+    </Modal>
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal.vue';
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+    Modal,
+  }
+};
+</script>
---
+++ 29.modal/src/components/Modal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-screen h-screen bg-blue-300 ">
+    c
+    <div class="bg-black/50 w-full h-full absolute top-0 left-0">
+a
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Modal",
+  }
+</script>

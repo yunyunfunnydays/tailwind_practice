e5cd26407719ecc0509edd3015903b2fa9cdec8e
yunhsuan <yunhsuan2017@gmail.com>
2023-04-14 17:10:21 +0800

    add 30

--- 29.modal/src/App.vue
+++ 29.modal/src/App.vue
@@ -3,22 +3,30 @@
     'w-screen h-screen',
     'p-5',
   ]">
-    <Modal>
-      
+    <RabbitButton class="w-40" text="open" color="yellow"     
+      @click="showed = true">
+    </RabbitButton>
+    <Modal title="嗨" :showModal="showed" @closing="showed = false">
+      <template v-slot:itemText  >
+        你好
+      </template>
     </Modal>
   </div>
 </template>
 
 <script>
 import Modal from './components/Modal.vue';
+import RabbitButton from './components/RabbitButton.vue'
 
 export default {
   data() {
     return {
+      showed: true,
     }
   },
   components: {
     Modal,
+    RabbitButton
   }
 };
 </script>
